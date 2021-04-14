0cc95e9289d99e97d066796a55d7fdf6e1543948
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2021-04-13 23:47:20 -0500

    Test database created, eviroment variables and tests with actions that use firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,16 +4,23 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDBAtUCo5KLBCJBtEKRYEzuZ-0R9hbUEw',
-  authDomain: 'react-app-firebase-bcdda.firebaseapp.com',
-  projectId: 'react-app-firebase-bcdda',
-  storageBucket: 'react-app-firebase-bcdda.appspot.com',
-  messagingSenderId: '1007486845048',
-  appId: '1:1007486845048:web:c83bca82ac08267d7f6d8d',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
+// if (process.env.NODE_ENV === 'test') {
+//   // Initialize Firebase in Testing
+//   firebase.initializeApp(firebaseConfigTesting);
+// } else {
+//   // Initialize Firebase in Development
+// }
+
 firebase.initializeApp(firebaseConfig);
+// console.log(process.env);
 
 //The database
 const db = firebase.firestore();
--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -4,19 +4,90 @@
 
 import configureStore from 'redux-mock-store'; //ES6 modules
 import thunk from 'redux-thunk';
-import { startNewNote } from '../../actions/notes';
+import {
+  startLoadingNotes,
+  startNewNote,
+  startSaveNote,
+} from '../../actions/notes';
+import { db } from '../../firebase/firebaseConfig';
+import { types } from '../../types/types';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-const store = mockStore({
+const initState = {
   auth: {
     uid: 'testing',
   },
-});
+};
+
+let store = mockStore(initState);
 
 describe('Tests in notes-actions', () => {
+  beforeEach(() => {
+    store.clearActions();
+  });
+
   test('should start new note', async () => {
     await store.dispatch(startNewNote());
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: types.notesActive,
+      payload: {
+        id: expect.any(String),
+        title: '',
+        body: '',
+        date: expect.any(Number),
+      },
+    });
+
+    expect(actions[1]).toEqual({
+      type: types.notesAddNew,
+      payload: {
+        id: expect.any(String),
+        title: '',
+        body: '',
+        date: expect.any(Number),
+      },
+    });
+
+    const docId = actions[0].payload.id;
+    await db.doc(`testing/journal/notes/${docId}`).delete();
+  });
+
+  test('should load notes', async () => {
+    await store.dispatch(startLoadingNotes('testing'));
+    const action = store.getActions();
+
+    expect(action[0]).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Array),
+    });
+
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    };
+
+    expect(action[0].payload[0]).toMatchObject(expected);
+  });
+
+  test('should save note', async () => {
+    const note = {
+      id: '7X8hxcFWLsXBDhGmkuF3',
+      title: 'title',
+      body: 'body',
+    };
+
+    await store.dispatch(startSaveNote(note));
+
+    const actions = store.getActions();
+    expect(actions[0].type).toBe(types.notesUpdated);
+
+    const docRef = await db.doc(`/testing/journal/notes/${note.id}`).get();
+    expect(docRef.data().title).toBe(note.title);
   });
 });
